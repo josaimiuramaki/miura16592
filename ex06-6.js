9813ba51ddb89da2e7fcb143c40e52676b0dd99a
zm16592 <zm16592@josai.ac.jp>
2018-10-21 21:08:56 -0400

    message

---
+++ ex06-6.js
@@ -0,0 +1,12 @@
+var who65='world';
+function sayhello65(){
+alert('Hello, ' + who65 + '!');
+}
+function taro(){
+  alert('太郎が来た, ' + who65 + '!');
+who65 = '太郎';
+}
+function hanako(){
+  alert('花子が来た, ' + who65 + '!');
+who65 = '花子';
+}
