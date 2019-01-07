bad9edc12d0577cce6793d587c3e1a3ee4f6e21c
zm16592 <zm16592@josai.ac.jp>
2019-01-06 20:49:53 -0500

    message

--- ex06-9.js
+++ ex06-9.js
@@ -1,13 +1,7 @@
 function txtchange1(){
-document.getElementById('text1').innerHTML = 'ナンバーワン！';
-
-}
-function imgchange1(){
-document.getElementById('logo').src = './image/jokoma.png';
-
+  document.getElementById('txt1').innerHTML = 'ナンバーワン！';
 }
 
-function imgchange2(){
-document.getElementById('logo').src = './image/JosaiLogo.png';
-
+function imgchange1() {
+  document.getElementById('logo').src = './image/jokoma.png';
 }
